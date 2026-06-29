89f7ec63c67b5b312c1507abd1059a86915a4167
sharath-kashyap <ksharath28@gmail.com>
2026-06-29 14:44:26 +0530

    Add settings for renamed multi-role Playwright framework folder

---
+++ multi_login_test_fixtures/config/settings.py
@@ -0,0 +1,17 @@
+BASE_URL = "https://app.example.com"
+AUTH_STATE_DIR = "multi_login_test_fixtures/.auth"
+
+USERS = {
+    "admin": {
+        "username": "admin_user",
+        "password": "admin_secret"
+    },
+    "user": {
+        "username": "normal_user",
+        "password": "user_secret"
+    },
+    "manager": {
+        "username": "manager_user",
+        "password": "manager_secret"
+    }
+}
